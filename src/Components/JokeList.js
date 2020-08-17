4481019edbe18783698c13ba7103e072c73a4a1c
Brian <brian.holland.pr@gmail.com>
2020-08-17 13:54:22 -0700

    added joke comp

--- src/Components/JokeList.js
+++ src/Components/JokeList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import Joke from './Joke';
 import axios from 'axios';
+import './JokeList.css';
 
 export class JokeList extends Component {
 	static defaultProps = {
@@ -17,7 +19,7 @@ export class JokeList extends Component {
 		while (jokes.length < this.props.numJokesToGet) {
 			//get a joke and change header to work with the api
 			let res = await axios.get('https://icanhazdadjoke.com', { headers: { Accept: 'application/json' } });
-			jokes.push(res.data.joke);
+			jokes.push({ text: res.data.joke, votes: 0 });
 			this.setState({ jokes: jokes });
 		}
 	}
@@ -25,11 +27,21 @@ export class JokeList extends Component {
 	render() {
 		return (
 			<div className="JokeList">
-				<ul>
-					{this.state.jokes.map((joke) => {
-						return <li>{joke}</li>;
+				<div className="JokeList-sidebar">
+					<h1 className="JokeList-title">
+						<span>Dad</span> Jokes
+					</h1>
+					<img
+						src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
+						alt="laughing emoji"
+					/>
+					<button className="JokeList-getmore">New Jokes</button>
+				</div>
+				<div className="JokeList-jokes">
+					{this.state.jokes.map((j) => {
+						return <Joke text={j.text} votes={j.votes} />;
 					})}
-				</ul>
+				</div>
 			</div>
 		);
 	}
